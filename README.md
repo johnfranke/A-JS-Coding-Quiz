5582913ac048b23f1ca829bfe4e388aa85e5c502
johnfranke <57201783+johnfranke@users.noreply.github.com>
2020-01-07 14:10:42 -0600

    Add files via upload
    adding homework04 files

---
+++ README.md
@@ -0,0 +1,10 @@
+# Code Quiz 
+
+## Description
+This is a code quiz I created by using JavaScript, HTML, and Bootstrap/CSS. 
+
+The user is promted with a landing screen that welcomes them to the code and invites them to click the start button to begin the quiz. Once clicked the time starts counting down from 75. If the user gets the question correctly five points will be added to their score. If the user answers the question incorrectly they will loose 15 secounds from their time. Depending on whether the user answers the question correctly or incorrectly, the user will see 'Right!' or 'Wrong!' pop up under the questions then quickly dissappear. 
+
+After the user had answered all 5 questions they will see their final score and be prompted to add their initials to the High Scores list. They may input their initials and hit sumbit. Upon submitting their initials they will be directed to the high scores and see their initials with their score added to it. The user then has two options, they can click on the clear high scores button which clears the high scores or they can click the 'back' button to go back to the beginning 'start' screen.
+
+You may view the quiz here: https://johnfranke.github.io/
---
+++ script.js
@@ -0,0 +1,216 @@
+var questions = [
+    {
+        questionTitle: "Commonly used data types DO NOT include:",
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        choice4: "numbers",
+        correct: "C"
+    },
+    {
+        questionTitle: "The condition in an if / else statement is enclosed within ____.",
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parentheses",
+        choice4: "square brackets",
+        correct: "C"
+    },
+    {
+        questionTitle: "Arrays in Javascript can be used to store ____.",
+        choice1: "number and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        correct: "D"
+    },
+    {
+        questionTitle: "String values must be enclosed within ____ when being assigned to variables.",
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parenthese",
+        correct: "C"
+    },
+    {
+        questionTitle: "A very useful tool in development and debugging for printing content to the debugger",
+        choice1: "JavaScript",
+        choice2: "terminal/bash",
+        choice3: "for loops",
+        choice4: "console.log",
+        correct: "D"
+    }
+]
+
+
+var start = document.querySelector("#start");
+var sec = 75;
+var quiz = document.querySelector("#quiz");
+var thetime = document.querySelector("#thetime");
+var viewHighscores = document.querySelector("#viewhighscores")
+var questionTitle = document.querySelector("#title");
+var choice1 = document.querySelector("#A");
+var choice2 = document.querySelector("#B");
+var choice3 = document.querySelector("#C");
+var choice4 = document.querySelector("#D");
+var correctIncorrect = document.querySelector("#rightorwrong");
+var addInitials = document.querySelector("#addinitials");
+var highScores = document.querySelector('#highscores');
+var clearHighscores = document.querySelector('#clearhighscores-button');
+var goBack = document.querySelector('#goback-button');
+var topRow = document.querySelector('#toprow');
+var highScoreList = document.querySelector('#highscoreslist');
+
+var lastQuestionIndex = questions.length - 1;
+var currentQuestionIndex = 0;
+const questionTime = 15;
+var count = 75;
+var score = 0;
+var TIMER;
+
+
+function showQuestion() {
+    var currentQ = questions[currentQuestionIndex];
+    title.innerHTML = "<h4>" + currentQ.questionTitle + "</h4>";
+    choice1.innerHTML = currentQ.choice1;
+    choice2.innerHTML = currentQ.choice2;
+    choice3.innerHTML = currentQ.choice3;
+    choice4.innerHTML = currentQ.choice4;
+    console.log(score);
+}
+
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec;
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        showScore();
+    }
+}
+
+start.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    start.style.display = "none";
+    myTimer()
+    time = setInterval(myTimer, 1000);
+    showQuestion();
+    quiz.style.display = "block";
+
+}
+
+function checkAnswer(answer){
+    if( answer === questions[currentQuestionIndex].correct){
+        // answer is correct
+        score += 5;
+        correctAnswer();
+    }else{
+        // answer is wrong
+        sec -= 15;
+         wrongAnswer();
+    }
+    count = 0;
+    if(currentQuestionIndex < lastQuestionIndex){
+        currentQuestionIndex++;
+        showQuestion();
+    }else{
+        // end the quiz and show the score
+        clearInterval(time);
+        showScore();
+    }
+}
+
+
+function showScore(){
+    quiz.style.display = "none";
+    start.style.display = "none";
+    topRow.style.display = "none";
+    scoreContainer.style.display = "block";
+    document.getElementById('finalscore').innerHTML = score;
+}
+
+
+function correctAnswer() {
+    var removeAfter = 0;
+        $('#rightorwrong').append("<p>" + 'Right!' + "</p>");
+        removeAfter += 600;
+        (function (removeAfter) {
+            setTimeout(function () {
+                $("#rightorwrong").children().last().remove();
+            }, removeAfter);
+        })(removeAfter);
+    }
+
+
+function wrongAnswer() {
+    var removeAfter = 0;
+        $('#rightorwrong').append("<p>" + 'Wrong!' + "</p>");
+        removeAfter += 600;
+        (function (removeAfter) {
+            setTimeout(function () {
+                $("#rightorwrong").children().last().remove();
+            }, removeAfter);
+        })(removeAfter);
+    }
+
+    addInitials.addEventListener("click", function(event) {
+        event.preventDefault();
+      
+        var initials = document.querySelector("#initials").value;
+        
+        if (initials === "") {
+          alert("Initials cannot be blank");
+
+        } else {
+
+          localStorage.setItem("initials", initials);
+          localStorage.setItem("score", score);
+          showHighscores();
+        }
+        console.log(initials);
+        console.log(score);
+
+      });
+
+      function showHighscores() {
+        quiz.style.display = "none";
+        scoreContainer.style.display = "none";
+        thetime.style.display = "none";
+        viewHighscores.style.display = "none";
+        start.style.display = "none";
+        topRow.style.display = "none";
+        highScores.style.display = "block";
+        
+        var initials = localStorage.getItem("initials");
+        var score = localStorage.getItem("score");
+
+        console.log(initials);
+        console.log(score);
+
+        if (localStorage.value = "") {
+            document.getElementById("highscoreslist").innerHTML = "";
+            // var highScoreList = document.getElementById("highscoreslist");
+            // highScoreList.remove();
+          } else {
+               $('#highscoreslist').appendChild("<p>", initials + " ............... Score: ", + score, "</p>");
+
+          }
+      }
+
+      clearHighscores.addEventListener("click", function(event) {
+        localStorage.clear();
+        document.getElementById("highscoreslist").innerHTML = "";
+
+      });
+
+      goBack.addEventListener("click", function(event) {
+        event.preventDefault();
+        showQuestion()
+      
+     
+      });
+
+      viewHighscores.addEventListener("click", function(event) {
+            showHighscores()
+                 
+     
+      });
